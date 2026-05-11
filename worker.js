77246ed878cf8a555aacf44e5e38f305260750cd
Zulfikar Muhamad <zul.m@icloud.com>
2026-05-11 17:21:54 +0800

    Add worker script to redirect all traffic to /maintenance

---
+++ worker.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (!url.pathname.startsWith("/maintenance")) {
+      return Response.redirect(new URL("/maintenance", url), 302);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
